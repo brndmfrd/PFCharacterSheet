17530da5ba644dd25b84a7fcb237517037605eec
Bryan Ford <brndmfrd@gmail.com>
2015-06-21 18:21:15 -0400

    Filled out static values of ActiveCharacter class

--- ActiveCharacter/ActiveCharacter.cs
+++ ActiveCharacter/ActiveCharacter.cs
@@ -8,6 +8,52 @@ namespace ActiveCharacter
 {
     public static class ActiveCharacter
     {
-        public static string dummyString = "Active Character static string";
+        // --- character information ---
+        public static int ID = 0;
+        public static string characterName = "noval";
+        public static string playerName = "noval";
+        public static string race = "noval";
+        public static string race = "noval";
+        public static string size = "noval";
+        public static string alignment = "noval";
+        public static string age = "noval";
+        public static string deity = "noval";
+        public static string gender = "noval";
+        public static string height = "noval";
+        public static string weight = "noval";
+        public static string eyes = "noval";
+        public static string hair = "noval";
+        public static string homeland = "noval";
+        public static int strength = 0;
+        public static int dextarity = 0;
+        public static int constitution = 0;
+        public static int intelligence = 0;
+        public static int wisdom = 0;
+        public static int charisma = 0;
+        public static string languages = "noval";
+        public static string experiencePoints = "noval";
+
+        // --- combat---
+        public static string hitPoints = "noval";
+        public static string initiative = "noval";
+        public static string armorClass = "noval";
+        public static string fortitudeSave = "noval";
+        public static string reflexSave = "noval";
+        public static string willSave = "noval";
+        public static string baseAttackBonus = "noval";
+        public static string combatManeuverBonus = "noval";
+        public static string combatManeuverDefence = "noval";
+        public static string statusEffect = "noval";
+
+        // --- gear and wealth ---
+        public static string gold = "noval";
+        public static string gear = "noval";
+        public static string equipment = "noval";
+        public static string weapons = "noval";
+        public static string armor = "noval";
+
+
+
+
     }
 }
