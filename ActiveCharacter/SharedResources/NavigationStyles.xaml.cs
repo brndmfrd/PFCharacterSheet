bb2fa473d5325cc6aff471a26fd08e955073a36e
Bryan Ford <brndmfrd@gmail.com>
2015-06-23 18:52:27 -0400

    Added navigationStyles to our Shared Resources

--- ActiveCharacter/SharedResources/NavigationStyles.xaml.cs
+++ ActiveCharacter/SharedResources/NavigationStyles.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace NavigationBarModule.Resources
+namespace ActiveCharacter.SharedResources
 {
     /// <summary>
     /// Interaction logic for NavigationStyles.xaml
