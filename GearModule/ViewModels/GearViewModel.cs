004badeb160fdf1287806137d8ffe9145161055e
Bryan Ford <brndmfrd@gmail.com>
2015-06-25 00:18:35 -0400

    Begin designing GearModule

--- GearModule/ViewModels/GearViewModel.cs
+++ GearModule/ViewModels/GearViewModel.cs
@@ -4,13 +4,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Practices.Prism.Regions;
+using ActiveCharacter;
+using System.Windows.Input;
 
 
 namespace GearModule.ViewModels
 {
-    public class GearViewModel: INavigationAware
+    public class GearViewModel : ObservableObject, INavigationAware
     {
-        public GearViewModel() { }
+        public string _amount = "0";
+
+        public string Gear
+        {
+            get { return Character.gear; }
+            set
+            {
+                Character.gear = value;
+                RaisePropertyChangedEvent("Gear");
+            }
+        }
+
+        public string Amount
+        {
+            get { return _amount; }
+            set
+            {
+                _amount = value;
+                RaisePropertyChangedEvent("Amount");
+            }
+        }
+
+
+
+        // ---------------------------
+        public ICommand ChangeGearCommand { get { return new DelegateCommand(UpdateGear); } }
+        public ICommand ChangeAmountCommand { get { return new DelegateCommand(UpdateAmount); } }
+
+
+        // ---------------------------
+        public void UpdateGear() { Gear = "Lizard Sabar of Justice"; }
+        public void UpdateAmount() { Amount = "42"; }
+
 
 
         // ------- needed for INavigationAware -------
@@ -23,6 +57,6 @@ namespace GearModule.ViewModels
         public bool IsNavigationTarget(NavigationContext navigationContext)
         {
             return true;
-        }
+        }        
     }
 }
