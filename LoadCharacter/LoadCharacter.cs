f1c88700954c4282cdcdd7103f505ba6a24fe398
Bryan Ford <brndmfrd@gmail.com>
2015-09-09 21:29:38 -0400

    Began character loading feature
    Allows loading of character files from the disk into the character
    sheet.

---
+++ LoadCharacter/LoadCharacter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LoadCharacter
+{
+    public class LoadCharacter
+    {
+        // current working directory
+        string cdir;        // full path of our cwd
+        string file;        // our character file to load
+        string[] pfFiles;   // list of files in cwd
+
+        /// <summary>
+        /// Constructor. Sets the character file as the first pathfinder file in the CWD by default. 
+        /// </summary>
+        public LoadCharacter()
+        {
+            cdir = Directory.GetCurrentDirectory();
+            file = GetFirstPfFile();
+        }
+
+        /// <summary>
+        /// Return the fist pathfinder file in the cwd
+        /// </summary>
+        /// <returns></returns>
+        private string GetFirstPfFile()
+        {
+            // We want the files that end with .pf
+            pfFiles = Directory.GetFiles(cdir, "*.pf");
+
+            return pfFiles.FirstOrDefault();
+        }
+
+        
+
+        
+    }
+}
