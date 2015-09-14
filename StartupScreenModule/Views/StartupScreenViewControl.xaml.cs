ca0aa176e0e992fbe3665fc6180f5108b050be5f
Bryan Ford <brndmfrd@gmail.com>
2015-09-13 20:13:28 -0400

    Added panels to character file loading ui
    Added event handlers on buttons click, buttons to load new objects for
    new stack panels, more options to load character file.

--- StartupScreenModule/Views/StartupScreenViewControl.xaml.cs
+++ StartupScreenModule/Views/StartupScreenViewControl.xaml.cs
@@ -22,7 +22,9 @@ namespace StartupScreenModule.Views
     public partial class StartupScreenViewControl : UserControl
     {
         // Deals with UI elements specific to loading a character
-        Views.UiElements uiElements;
+        // todo:  do these need to be sepearate objects?
+        Views.UiElements dragDropObject;
+        Views.UiElements loadCharacterOptions;
 
 
         /// <summary>
@@ -31,8 +33,20 @@ namespace StartupScreenModule.Views
         public StartupScreenViewControl()
         {
             InitializeComponent();
-            
-            uiElements = new Views.UiElements();            
+
+            // --- todo: make a seperate method for all this ui object creation ---
+            dragDropObject = new Views.UiElements();
+            loadCharacterOptions = new Views.UiElements();
+
+            // Load character options object
+            loadCharacterOptions.CreateButtons();
+            loadCharacterOptions.defaultLocationButton.Click += Load_Default_Button_Click;
+            loadCharacterOptions.explorerButton.Click += Load_Explorer_Button_Click;
+            loadCharacterOptions.fileDropButton.Click += Load_DragDrop_Button_Click;
+
+            // Drag/Drop object
+            dragDropObject.CreateFileDropTextBlock();
+            dragDropObject.fileDropTextBlock.AllowDrop = true;
         }
 
 
@@ -46,66 +60,102 @@ namespace StartupScreenModule.Views
             if (e.Data.GetDataPresent(DataFormats.FileDrop, true))
             {        
                 // Get the path of the dropped file
-                string[] filePathOfDroppedFile = e.Data.GetData(DataFormats.FileDrop, true) as string[];
+                string[] filePathOfDroppedFile = 
+                    e.Data.GetData(DataFormats.FileDrop, true) as string[];
                               
                 // Add path to filedroplbl to display path to user
-                uiElements.filedropTextBlock.Text += string.Format("Attempting to load file: \n {0}\n", filePathOfDroppedFile[0]);
+                dragDropObject.fileDropTextBlock.Text += 
+                    string.Format("Attempting to load file: \n {0}\n", filePathOfDroppedFile[0]);
 
-                if (ConfirmFileLoad(filePathOfDroppedFile[0]))
+                // Open message box to confirm with user path is correct.
+                if (dragDropObject.ConfirmFileLoad(filePathOfDroppedFile[0]))
                 {
-                    // Tell Model what the path of the file dropped is
-                    Models.LoadCharacter.CharacterfileAndPath = filePathOfDroppedFile[0];
+                    Models.LoadCharacter.CharacterfileAndPath = filePathOfDroppedFile[0];   // Tell our Model the path of the file dropped
                 }
                 else
                 {
-                    uiElements.filedropTextBlock.Text += @"File load cancelled.  No action taken.";
+                    dragDropObject.fileDropTextBlock.Text += @"File load cancelled.  No action taken.\n\n";
                 }
             }            
         }
 
-        /// <summary>
-        /// Confirm with the user that the path they have provided is one they would like to attempt 
-        /// to load.
-        /// </summary>
-        /// <param name="path"></param>
-        /// <returns></returns>
-        private bool ConfirmFileLoad(string path)
-        {
-            var result =
-                MessageBox.Show(string.Format("Load {0}?", path),
-                "Please Confirm File", 
-                MessageBoxButton.OKCancel, 
-                MessageBoxImage.Question, 
-                MessageBoxResult.Cancel
-                );
-
-            if (result == MessageBoxResult.OK)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }            
-        }
-
 
         /// <summary>
-        /// On click display the file-drop space for users to drag/drop files to used as their character. 
-        /// The filedroplbl will capture the full path to the file drag/dropped.
+        /// Event handler to load objects into the character loader options panel.  
+        /// This panel propigates buttons for the user to select the method to load in their character file.
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Load_Character_Button_Click(object sender, RoutedEventArgs e)
         {
-            if (Sp1FileDrop.Children.Contains(uiElements.filedropTextBlock))
+            if (Sp1LoadOptions.Children.Contains(loadCharacterOptions.explorerButton) &&
+                Sp1LoadOptions.Children.Contains(loadCharacterOptions.defaultLocationButton) && 
+                Sp1LoadOptions.Children.Contains(loadCharacterOptions.fileDropButton))
             {
-                Sp1FileDrop.Children.Remove(uiElements.filedropTextBlock);
+                Sp1LoadOptions.Children.Remove(loadCharacterOptions.explorerButton);
+                Sp1LoadOptions.Children.Remove(loadCharacterOptions.defaultLocationButton);
+                Sp1LoadOptions.Children.Remove(loadCharacterOptions.fileDropButton);
             }
             else
             {
-                Sp1FileDrop.Children.Add(uiElements.filedropTextBlock);
+                Sp1LoadOptions.Children.Add(loadCharacterOptions.explorerButton);
+                Sp1LoadOptions.Children.Add(loadCharacterOptions.defaultLocationButton);
+                Sp1LoadOptions.Children.Add(loadCharacterOptions.fileDropButton);
             }
         }
+
+
+        /// <summary>
+        /// Event handler when the user selects to load their character from one of 
+        /// the files from the default file directories.  This location will likely be the same directory where the 
+        /// application executable is saved.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Load_Default_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Sp2LoaderPanel.Children.Contains(dragDropObject.fileDropTextBlock))
+            {
+                Sp2LoaderPanel.Children.Remove(dragDropObject.fileDropTextBlock);
+            }
+            else
+            {
+                Sp2LoaderPanel.Children.Add(dragDropObject.fileDropTextBlock);
+            }
+        }
+
+
+        /// <summary>
+        /// Event handler when the user elects to select their character by using the native file explorer for 
+        /// their operating system.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Load_Explorer_Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+
+        /// <summary>
+        /// Event handler when the user elects to drag/drop their charcter file into the application.  
+        /// This panel is propigated with a textbox where the user may drag/drop into.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Load_DragDrop_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Sp2LoaderPanel.Children.Contains(dragDropObject.fileDropTextBlock))
+            {
+                Sp2LoaderPanel.Children.Remove(dragDropObject.fileDropTextBlock);
+            }
+            else
+            {
+                Sp2LoaderPanel.Children.Add(dragDropObject.fileDropTextBlock);
+            }
+        }
+
+
+
     }
 }
--- StartupScreenModule/Views/UiElements.cs
+++ StartupScreenModule/Views/UiElements.cs
@@ -12,23 +12,79 @@ namespace StartupScreenModule.Views
     /// </summary>
     public class UiElements
     {
-        public TextBlock filedropTextBlock;
+        public TextBlock fileDropTextBlock;
+        public Button defaultLocationButton;
+        public Button explorerButton;
+        public Button fileDropButton;
 
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
         public UiElements()
         {
-            CreateFileDropTextBlock();
         }
 
-        private void CreateFileDropTextBlock()
+
+        public void CreateFileDropTextBlock()
         {
-            filedropTextBlock = new TextBlock();
-            filedropTextBlock.Name = "FileDropTextBlock";
-            filedropTextBlock.Width = 400;
-            filedropTextBlock.Height = 336;
+            fileDropTextBlock = new TextBlock();
+            fileDropTextBlock.Name = "FileDropTextBlock";
+            fileDropTextBlock.Width = 400;
+            fileDropTextBlock.Height = 336;
             //filedroplbl.HorizontalContentAlignment = HorizontalAlignment.Center;
-            filedropTextBlock.Foreground = Brushes.GreenYellow;
-            filedropTextBlock.Text = "Drag/Drop a file here.\n\n";
-            filedropTextBlock.TextWrapping = TextWrapping.Wrap;
+            fileDropTextBlock.Foreground = Brushes.GreenYellow;
+            fileDropTextBlock.Text = "Drag/Drop a file here.\n\n";
+            fileDropTextBlock.TextWrapping = TextWrapping.Wrap;            
+        }
+
+
+        /// <summary>
+        /// Confirm with the user that the path they have provided is one they would like to attempt 
+        /// to load.
+        /// </summary>
+        /// <param name="path">File path to character file.</param>
+        /// <returns></returns>
+        public bool ConfirmFileLoad(string path)
+        {
+            var result =
+                MessageBox.Show(string.Format("Load {0}?", path),
+                "Please Confirm File",
+                MessageBoxButton.OKCancel,
+                MessageBoxImage.Question,
+                MessageBoxResult.Cancel
+                );
+
+            if (result == MessageBoxResult.OK)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+
+        internal void CreateButtons()
+        {
+            explorerButton = new Button();
+            explorerButton.Content = "Select From Windows Explorer";
+            explorerButton.Width = 200;
+            explorerButton.Height = 80;
+
+            defaultLocationButton = new Button();
+            defaultLocationButton.Content = "Select From Default Location";
+            defaultLocationButton.Width = 200;
+            defaultLocationButton.Height = 80;
+
+            fileDropButton = new Button();
+            fileDropButton.Content = "Drag/Drop File";
+            fileDropButton.Width = 200;
+            fileDropButton.Height = 80;
+
+
         }
     }
 }
