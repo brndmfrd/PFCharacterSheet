4c105f1e5d41a418fc4694e43bcac8155e51d01f
Bryan Ford <brndmfrd@gmail.com>
2015-11-28 20:44:14 -0500

    Additonal from last push
    Apparently github missed one.

--- SkillsModule/ViewModel/SkillsViewModel.cs
+++ SkillsModule/ViewModel/SkillsViewModel.cs
@@ -1,12 +1,28 @@
 ﻿using System.Collections.Generic;
 using Microsoft.Practices.Prism.Regions;
+using ModuleTools;
 using ActiveCharacter;
+using SkillsModule.Model;
+using SkillsModule.View;
 
 namespace SkillsModule.ViewModel
 {
-    class SkillsViewModel : INavigationAware
+    class SkillsViewModel : ObservableObject, INavigationAware
     {
         public IEnumerable<Skill> Skills { get { return Character.Skills; } }
+
+        // Listbox in primary view uses this accessor for what is has currently selected
+        // Navigation allows the Aux view to 'refresh' and update based on the selected value
+        public Skill MySelectedItem
+        {
+            get { return SelectedState.CurrentSelectedFeat; }
+            set
+            {
+                SelectedState.CurrentSelectedFeat = value;
+                RaisePropertyChangedEvent("MySelectedItem");
+                SkillsModule.regionManager.RequestNavigate("AuxContentRegion", typeof(View.SkillsViewAux).FullName);
+            }
+        }
 
         #region Navigation
         public void OnNavigatedFrom(NavigationContext navigationContext)
@@ -21,6 +37,7 @@ namespace SkillsModule.ViewModel
                 Character.Skills.Add(new Skill() { Name = "Slight of Hand", Ranks = 7, Description = "Street magician or pickpocket ability." });
                 Character.Skills.Add(new Skill() { Name = "Swim", Ranks = 4, Description = "How proficient you are in not drowning." });
             }
+            RaisePropertyChangedEvent("MySelectedItem");
         }
 
         public bool IsNavigationTarget(NavigationContext navigationContext)
