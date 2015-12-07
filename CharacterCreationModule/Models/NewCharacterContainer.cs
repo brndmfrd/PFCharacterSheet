754147ec59fbe556fe8897f828e7bcfe0747999e
Bryan Ford <brndmfrd@gmail.com>
2015-12-07 12:08:13 -0500

    Groundwork for CharacterCreation.BasicInfo
    General style to character data entry.

---
+++ CharacterCreationModule/Models/NewCharacterContainer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ActiveCharacter;
+using System.Collections.ObjectModel;
+
+namespace CharacterCreationModule.Models
+{
+    public static class NewCharacterContainer
+    {
+        #region Character Information
+        /// <summary>
+        /// The player chosen characteristics of the character.
+        /// </summary>        
+        public static ObservableCollection<BasicInformation> Information = new ObservableCollection<BasicInformation>();
+
+        /// <summary>
+        /// Player rolled ability scores
+        /// </summary>
+        public static ObservableCollection<Abilities> AbilityScores = new ObservableCollection<Abilities>();
+        #endregion Character Information
+
+        #region Languages
+        /// <summary>
+        /// Player chosen languages the character can speak fluently.
+        /// </summary>
+        public static ObservableCollection<string> Languages = new ObservableCollection<string>() { };
+        #endregion Languages
+
+        #region Classes
+        /// <summary>
+        /// Player chosen classes and associated levels.
+        /// </summary>
+        public static ObservableCollection<CharacterClass> Classes = new ObservableCollection<CharacterClass>() { };
+        #endregion Classes
+
+        #region Gear
+        /// <summary>
+        /// The stuff you carry or have equipped.
+        /// </summary>
+        public static ObservableCollection<Item> Gear = new ObservableCollection<Item>() { };
+        #endregion Gear
+
+        #region Skills
+        /// <summary>
+        /// Player chosen skills and associated rank points.
+        /// </summary>
+        public static ObservableCollection<Skill> Skills = new ObservableCollection<Skill>() { };
+        #endregion Skills
+
+        /// <summary>
+        /// Player chosen Feats added to this character
+        /// </summary>
+        public static ObservableCollection<Feat> Feats = new ObservableCollection<Feat>() { };
+
+        /// <summary>
+        /// Player chosen Spells added to this character
+        /// </summary>       
+        public static ObservableCollection<Spell> Spells = new ObservableCollection<Spell>() { };
+    }
+}
---
+++ CharacterCreationModule/ViewModels/CharacterCreation.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ActiveCharacter;
+using ModuleTools;
+using Microsoft.Practices.Prism.Regions;
+
+namespace CharacterCreationModule.ViewModels
+{
+    public class CharacterCreation : ObservableObject, INavigationAware
+    {
+        public IEnumerable<BasicInformation> BasicInformation { get { return Models.NewCharacterContainer.Information; } }
+        public IEnumerable<Abilities> Abilities { get { return Models.NewCharacterContainer.AbilityScores; } }
+        public IEnumerable<string> Languages { get { return Models.NewCharacterContainer.Languages; } }
+        public IEnumerable<CharacterClass> CharacterClasses { get { return Models.NewCharacterContainer.Classes; } }
+        public IEnumerable<Item> Items { get { return Models.NewCharacterContainer.Gear; } }
+        public IEnumerable<Skill> Skills { get { return Models.NewCharacterContainer.Skills; } }
+        public IEnumerable<Feat> Feats { get { return Models.NewCharacterContainer.Feats; } }
+        public IEnumerable<Spell> Spells { get { return Models.NewCharacterContainer.Spells; } }
+
+
+        #region Navigation
+        public void OnNavigatedFrom(NavigationContext navigationContext)
+        {
+
+        }
+        public void OnNavigatedTo(NavigationContext navigationContext)
+        {
+            // If we have no character data load these defaults (for testing purposes only, remove later)
+            if (Models.NewCharacterContainer.Information.Count <= 0)
+            {
+                Models.NewCharacterContainer.Information.Add(new BasicInformation() { Name = "Character Name", Value = "Duraxis" });
+                Models.NewCharacterContainer.Information.Add(new BasicInformation() { Name = "Player Name", Value = "Bryan" });
+                Models.NewCharacterContainer.Information.Add(new BasicInformation() { Name = "Size", Value = "M" });
+                Models.NewCharacterContainer.Information.Add(new BasicInformation() { Name = "Alignment", Value = "NG" });
+                Models.NewCharacterContainer.Information.Add(new BasicInformation() { Name = "Age", Value = "30" });
+            }
+        }
+        public bool IsNavigationTarget(NavigationContext navigationContext)
+        {
+            return true;
+        }
+        #endregion Navigation
+
+
+    }
+}
