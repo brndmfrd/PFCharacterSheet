a36d734146f58fcb548cfb7ac9b214ee421c9c07
Bryan Ford <brndmfrd@gmail.com>
2015-12-12 21:20:04 -0500

    Implementation of New Character - Save

--- CharacterCreationModule/ViewModels/CharacterCreation.cs
+++ CharacterCreationModule/ViewModels/CharacterCreation.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using ActiveCharacter;
 using ModuleTools;
 using Microsoft.Practices.Prism.Regions;
+using System.Windows.Input;
 
 namespace CharacterCreationModule.ViewModels
 {
@@ -19,6 +20,64 @@ namespace CharacterCreationModule.ViewModels
         public IEnumerable<Skill> Skills { get { return Models.NewCharacterContainer.Skills; } }
         public IEnumerable<Feat> Feats { get { return Models.NewCharacterContainer.Feats; } }
         public IEnumerable<Spell> Spells { get { return Models.NewCharacterContainer.Spells; } }
+
+        // ---- save character ----
+        private string saveFileName = "Default Name";
+        private string saveFilePath = "Default Path";
+        private string saveConfirmation;
+        public ICommand SaveCharacterCommand { get { return new DelegateCommand(SaveCharacter); } }
+        public ICommand SelectPathExplorerCommand { get { return new DelegateCommand(SelectPathExplorer); } }
+        
+        public string SaveFileName { get { return saveFileName; } set { saveFileName = value; RaisePropertyChangedEvent("SaveFileName"); } }
+        public string SaveFilePath { get { return saveFilePath; } set { saveFilePath = value; RaisePropertyChangedEvent("SaveFilePath"); } }
+        public string SaveConfirmation { get { return saveConfirmation; } set { saveConfirmation = value; RaisePropertyChangedEvent("SaveConfirmation"); } }
+
+        private void SaveCharacter()
+        {
+            SaveConfirmation = "Confirming Save, please wait--";
+            // todo:
+            // sanitize strings (file and path)
+            // perform file and path validation (can we write this file to this location?)
+            // write to file (or overwrite file) using json formatting
+            SaveConfirmation = string.Format("Success! \n File was saved at {0} \n File Name: {1} \n File Path: {2}", DateTime.Now, SaveFileName, SaveFilePath);
+        }
+        private void SelectPathExplorer()
+        {
+            // -- Open windows file explorer --
+            var ofd = new Microsoft.Win32.OpenFileDialog();
+            //ofd.DefaultExt = ".pf";
+            ofd.Multiselect = false;
+            ofd.DefaultExt = ".pf";
+            ofd.AddExtension = true;
+            ofd.Filter = "Pathfinder Files (*.pf)|*.pf|All Files (*.*)|*.*";
+            //ofd.InitialDirectory(SaveFilePath);
+            var success = ofd.ShowDialog();                     // returns nullable bool
+
+            // -- success is FALSE if user selects "Cancel" on the dialog
+            if (success != true)
+            {
+                return;
+            }
+
+            // True if the user has changed the file name
+            if (ofd.FileName.CompareTo(SaveFileName) != 0)
+            {
+                if (ofd.FileName.EndsWith(".pf"))
+                {
+                    var splitpath = ofd.FileName.Split('\\');
+                    SaveFileName = splitpath[splitpath.Length - 1];
+                    SaveFilePath = ofd.FileName.Substring(0, ofd.FileName.Length - SaveFileName.Length);
+                }
+                else
+                {
+                    var fullPath = ofd.FileName + ".pf";
+                    var splitpath = fullPath.Split('\\');
+                    SaveFileName = splitpath[splitpath.Length - 1];
+                    SaveFilePath = ofd.FileName.Substring(0, fullPath.Length - SaveFileName.Length);
+                }
+            }
+        }
+        // ----------------------------------
 
 
         #region Navigation
