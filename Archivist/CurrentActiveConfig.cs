d9b25a6d02d50d431b093be4bda10b5296d3664f
Bryan Ford <brndmfrd@gmail.com>
2016-01-30 18:59:40 -0500

    Load & Save safety controls added

--- Archivist/CurrentActiveConfig.cs
+++ Archivist/CurrentActiveConfig.cs
@@ -3,18 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Archivist
 {
     public static class CurrentActiveConfig
     {
-        //public static string filename = string.Empty;
-        //public static string saveDirectory = string.Empty;
-        //public static int? autoSaveFrequency = null;
-        //public static DateTime? lastSave = null;
-
-        public static string filename = "slappyMcGoo.pf";
-        public static string saveDirectory = "C:/Someplace";
+        public static string filename = "default.py";
+        public static string saveDirectory = Directory.GetCurrentDirectory();
         public static int? autoSaveFrequency = null;
         public static DateTime? lastSave = DateTime.Now;
     }
--- StartupScreenModule/ViewModels/OptionsViewModel.cs
+++ StartupScreenModule/ViewModels/OptionsViewModel.cs
@@ -9,23 +9,52 @@ namespace OptionsModule.ViewModels
 {
     public class OptionsViewModel : ObservableObject, INavigationAware
     {
-        public string _filename = "something";
+        #region Fields
+        private string _currentFile = CurrentActiveConfig.filename;
+        private bool loadButtonToggle = false;
+        private bool saveBeforeLoadToggle = false;
+        #endregion Fields
 
+        #region Properties
         public string SelectedItem { get; set; }
+
         public IEnumerable<string> FileList { get { return DirectoryMaster.DirectoryFileList; } }
+
         public string CurrentFile
         {
-            get { return _filename; }
-            set
+            get { return _currentFile; }
+            set 
             {
-                _filename = value;
-                RaisePropertyChangedEvent("CurrentFile");             
+                _currentFile = value; 
+                RaisePropertyChangedEvent("CurrentFile"); 
             }
         }
 
         public string CurrentDirectory { get { return CurrentActiveConfig.saveDirectory; } }
 
         public string LastSaveTimestamp { get { return CurrentActiveConfig.lastSave.ToString(); } }
+
+        public bool LoadButtonToggle
+        {
+            get { return loadButtonToggle; }
+            set
+            {
+                loadButtonToggle = value;
+                RaisePropertyChangedEvent("LoadButtonToggle");
+            }
+        }
+
+        public bool SaveBeforeLoadToggle
+        {
+            get { return saveBeforeLoadToggle; }
+            set
+            {
+                saveBeforeLoadToggle = value;
+                RaisePropertyChangedEvent("SaveBeforeLoadToggle");
+            }
+        }
+
+        #endregion Properties
 
         #region Commands
         public ICommand LoadNewCharacterCommand { get { return new DelegateCommand(PresentLoadCharacter); } }
@@ -34,10 +63,16 @@ namespace OptionsModule.ViewModels
 
         public ICommand SaveCharacterCommand { get { return new DelegateCommand(SaveCharacter); } }
 
+        public ICommand UndoCharacterCommand { get { return new DelegateCommand(UndoUserChanges); } }
+
         public ICommand ConfigCommand { get { return new DelegateCommand(PresentConfiguration); } }
 
         // User double-clicked a file name
         public ICommand SelectFileCommand { get { return new DelegateCommand(SelectFile); } }
+
+        public ICommand ComfirmLoadCommand { get { return new DelegateCommand(ConfirmLoad); } }
+
+        public ICommand SaveBeforeLoadCommand { get { return new DelegateCommand(SaveCharacter); } }
         #endregion Commands
 
         #region Command Methods
@@ -52,33 +87,43 @@ namespace OptionsModule.ViewModels
             OptionsModule.regionManager.RequestNavigate("AuxContentRegion", typeof(Views.SaveCharacter).FullName);
         }
 
+        private void UndoUserChanges()
+        {
+            CurrentFile = CurrentActiveConfig.filename;
+        }
+
         private void SaveCharacter()
         {
-            CurrentFile = "yo!";
-            return;
-            if (string.IsNullOrEmpty(CurrentFile))
+            if (string.IsNullOrEmpty(_currentFile))
             {
                 SaveCharacterFile.SaveCharacter();
             }
             else
             {
-                SaveCharacterFile.SaveCharacter(CurrentFile);
+                SaveCharacterFile.SaveCharacter(_currentFile);
             }
+
+            SaveBeforeLoadToggle = false; 
         }
 
         private void PresentConfiguration()
         {
             OptionsModule.regionManager.RequestNavigate("AuxContentRegion", typeof(Views.Configuration).FullName);
         }
-        #endregion Command Methods
 
         private void SelectFile()
         {
-            // todo: check if SelectedItem is null or irregular.
-            LoadCharacterFile.BeginLoadCharacterFile(SelectedItem);
+            LoadButtonToggle = true;
+            SaveBeforeLoadToggle = true;          
         }
 
-        
+        private void ConfirmLoad()
+        {
+            // todo: check if SelectedItem is null or irregular.
+            LoadCharacterFile.BeginLoadCharacterFile(SelectedItem);
+            LoadButtonToggle = false;
+        }
+        #endregion Command Methods        
 
         #region Navigation
         public void OnNavigatedFrom(NavigationContext navigationContext)
