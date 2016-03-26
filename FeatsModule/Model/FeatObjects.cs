f6f77787f724947c5b51643a78988c5061279434
Bryan Ford <brndmfrd@gmail.com>
2016-03-25 22:43:58 -0400

    Update Feats to use new Data Model

---
+++ FeatsModule/Model/FeatObjects.cs
@@ -0,0 +1,34 @@
+﻿using CharacterDataObjects;
+using System.Collections.ObjectModel;
+using SharedResources.EventHandlers;
+
+namespace FeatsModule.Model
+{
+
+    public class DisplayObject : ObservableObject
+    {
+        public string _name;
+        //public string _value;
+
+        public string Name { get { return _name; } set { _name = value; RaisePropertyChangedEvent("Name"); } }
+        //public string Value { get { return _value; } set { _value = value; RaisePropertyChangedEvent("Value"); } }
+    }
+
+    public class FeatObjects
+    {
+        public static ObservableCollection<DisplayObject> Feats
+        {
+            get
+            {
+                var tmp = new ObservableCollection<DisplayObject>();
+
+                foreach (var elem in MyCharacter.Feats)
+                {
+                    tmp.Add(new DisplayObject { Name = elem.Name});
+                }
+
+                return tmp;
+            }
+        }
+    }
+}
--- FeatsModule/ViewModel/FeatsViewModel.cs
+++ FeatsModule/ViewModel/FeatsViewModel.cs
@@ -1,46 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Microsoft.Practices.Prism.Regions;
-using ActiveCharacter;
-using SharedResources;
 using SharedResources.EventHandlers;
 using FeatsModule.Model;
-using System.Linq;
-using System.Windows.Input;
-using FeatsModule.View;
 
 namespace FeatsModule.ViewModel
 {
     class FeatsViewModel : ObservableObject, INavigationAware
     {
-        public IEnumerable<Feat> Feats { get { return Character.Feats; } }
+        public IEnumerable<DisplayObject> Feats { get { return FeatObjects.Feats; } }
 
         // Listbox in primary view uses this accessor for what is has currently selected
         // Navigation allows the Aux view to 'refresh' and update based on the selected value
-        public Feat MySelectedItem
-        {
-            get { return SelectedState.CurrentSelectedFeat; }
-            set { SelectedState.CurrentSelectedFeat = value; 
-                RaisePropertyChangedEvent("MySelectedItem"); 
-                FeatsModule.regionManager.RequestNavigate("AuxContentRegion", typeof(View.FeatsViewAux).FullName); 
-            }
-        }
+        //public Feat MySelectedItem
+        //{
+        //    get { return SelectedState.CurrentSelectedFeat; }
+        //    set { SelectedState.CurrentSelectedFeat = value; 
+        //        RaisePropertyChangedEvent("MySelectedItem"); 
+        //        FeatsModule.regionManager.RequestNavigate("AuxContentRegion", typeof(View.FeatsViewAux).FullName); 
+        //    }
+        //}
 
         #region Navigation
         public void OnNavigatedFrom(NavigationContext navigationContext)
         {
         }
+
         public void OnNavigatedTo(NavigationContext navigationContext)
         {
-            // If we have no character data load these defaults (for testing purposes only, remove later)
-            if (Character.Feats.Count <= 0)
-            {
-                Character.Feats.Add(new Feat() { Name = "Dodge", Description = "Dodge bonus +2 AC." });
-                Character.Feats.Add(new Feat() { Name = "Cleave", Description = "If you hit someone, make another attack." });
-                Character.Feats.Add(new Feat() { Name = "Maximize Spell", Description = "Makes your spells bigger." });
-            }
-
-            RaisePropertyChangedEvent("MySelectedItem");
         }
 
         public bool IsNavigationTarget(NavigationContext navigationContext)
