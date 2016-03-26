7cb92cb3806df677bade067e088342519065d95a
Bryan Ford <brndmfrd@gmail.com>
2016-03-26 17:24:13 -0400

    Added Test class for building up dummy data for UI/UX testing

---
+++ TestingTools/LoadCharacterObjectsWithData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingTools
+{
+    public class LoadCharacterObjectsWithData
+    {
+        public static bool IhaveBeenBuilt = false;
+
+        public LoadCharacterObjectsWithData()
+        {
+            IhaveBeenBuilt = true;
+        }
+
+        public void BuildUpNewCharacter()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
